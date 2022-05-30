26f1042cabdd04d5aaf2f6b761acf3120dbd5e08
amandamelodev <amanda.maciel@ulbra.inf.br>
2022-05-29 21:30:34 -0300

    consulta cep

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,12 @@
-$(document).ready(function(){
+$(document).ready(function () {
 
- 
+  $("#consult").click(function(){
+    var cep = $("#cep").val();
+
+    $.get("https://viacep.com.br/ws/"+cep+"/json/", function(data){
+      $("#result").html("A cidade é: " + data.localidade);
+    })
+    
+  });
 
 });
